6c0384b793dc37920dad556aa514cec778106117
Mike S <mstange@dthera.com>
2019-01-09 11:11:43 -0800

    tree solution

---
+++ python/tree.py
@@ -0,0 +1,10 @@
+print('hello world')
+
+def check_binary_search_tree(root):
+  if root.left == None and root.right == None
+    return True
+  
+  if root.left and check_binary_search_tree(root.left)
+
+
+check_binary_search_tree()
