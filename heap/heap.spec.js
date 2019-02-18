06dda75b172137db1e2e9426372ad4217818d9cd
Mike S <mstange@dthera.com>
2019-02-18 09:07:45 -0800

    max and min heap

--- heap/heap.spec.js
+++ heap/heap.spec.js
@@ -1,4 +1,5 @@
-import { MinHeap } from './heap.js';
+import { MinHeap } from './min-heap.js';
+import { MaxHeap } from './max-heap.js';
 
 describe('MinHeap', () => {
   test('empty heap', () => {
@@ -48,5 +49,76 @@ describe('MinHeap', () => {
     const minHeap = new MinHeap(1);
     minHeap.remove();
     expect(minHeap.elements).toEqual([]);
+  });
+  test('can remove all elements', () => {
+    const minHeap = new MinHeap(17, 15, 1, 10, 8, 20);
+    for (let i = 0; i < 6; i++) {
+      minHeap.remove();
+    }
+    expect(minHeap.elements).toEqual([]);
+  });
+});
+
+describe('MaxHeap', () => {
+  test('empty heap', () => {
+    const maxHeap = new MaxHeap();
+    expect(maxHeap.elements).toEqual([]);
+  });
+  test('can initialize heap', () => {
+    const maxHeap = new MaxHeap(1);
+    expect(maxHeap.elements).toEqual([1]);
+  });
+  test('can peek', () => {
+    const maxHeap = new MaxHeap(1);
+    expect(maxHeap.peek()).toEqual(1);
+  });
+  test('peek empty heap is an error', () => {
+    const maxHeap = new MaxHeap();
+    expect(() => {
+      maxHeap.peek();
+    }).toThrow(new Error('Empty heap'));
+  });
+  test('can initialize heap with multiple values', () => {
+    const maxHeap = new MaxHeap(17, 15, 1, 10, 8, 20);
+    expect(maxHeap.elements).toEqual([20, 15, 17, 10, 8, 1]);
+  });
+  test('can add element', () => {
+    const maxHeap = new MaxHeap();
+    maxHeap.add(1);
+    expect(maxHeap.elements).toEqual([1]);
+  });
+  test('can add two elements', () => {
+    const maxHeap = new MaxHeap();
+    maxHeap.add(1);
+    maxHeap.add(20);
+    expect(maxHeap.elements).toEqual([20, 1]);
+  });
+  test('can add multiple elements', () => {
+    const maxHeap = new MaxHeap();
+    maxHeap.add(17);
+    maxHeap.add(15);
+    maxHeap.add(1);
+    maxHeap.add(10);
+    maxHeap.add(8);
+    maxHeap.add(20);
+    expect(maxHeap.elements).toEqual([20, 15, 17, 10, 8, 1]);
+  });
+  test('remove on empty heap is an error', () => {
+    const maxHeap = new MaxHeap();
+    expect(() => {
+      maxHeap.remove();
+    }).toThrow(new Error('Empty heap'));
+  });
+  test('can remove element', () => {
+    const maxHeap = new MaxHeap(1);
+    maxHeap.remove();
+    expect(maxHeap.elements).toEqual([]);
+  });
+  test('can remove all elements', () => {
+    const maxHeap = new MaxHeap(17, 15, 1, 10, 8, 20);
+    for (let i = 0; i < 6; i++) {
+      maxHeap.remove();
+    }
+    expect(maxHeap.elements).toEqual([]);
   });
 });
---
+++ heap/max-heap.js
@@ -0,0 +1,88 @@
+const getParentIndex = index => Math.floor((index - 1) / 2);
+const getLeftChildIndex = index => index * 2 + 1;
+const getRightChildIndex = index => index * 2 + 2;
+
+export class MaxHeap {
+  constructor(...args) {
+    this.elements = [];
+    for (let i = 0; i < args.length; i++) {
+      this.add(args[i]);
+    }
+  }
+
+  hasLeftChild(index) {
+    return this.elements[(index * 2) + 1] !== undefined;
+  }
+
+  hasRightChild(index) {
+    return this.elements[(index * 2) + 2] !== undefined;
+  }
+
+  hasParent(index) {
+    return this.elements[Math.floor((index - 1) / 2)] !== undefined;
+  }
+  
+  leftChild(index) {
+    return this.elements[(index * 2) + 1];
+  }
+
+  rightChild(index) {
+    return this.elements[(index * 2) + 2];
+  }
+  
+  parent(index) {
+    return this.elements[Math.floor((index - 1) / 2)];
+  }
+
+  swap(index1, index2) {
+    const temp = this.elements[index1];
+    this.elements[index1] = this.elements[index2];
+    this.elements[index2] = temp;
+  }
+
+  peek() {
+    if (this.elements.length === 0) throw new Error('Empty heap');
+    return this.elements[0];
+  }
+
+  add(element) {
+    this.elements.push(element);
+    this.bubbleUp();
+  }
+
+  remove() {
+    if (this.elements.length === 0) throw new Error('Empty heap');
+    const item = this.elements[0];
+    if (this.elements.length === 1) {
+      this.elements.pop();
+    } else {
+      this.elements[0] = this.elements.pop();
+    }
+    this.bubbleDown();
+    return item;
+  }
+  
+  bubbleUp() {
+    let index = this.elements.length - 1;
+    while (this.hasParent(index) && this.elements[getParentIndex(index)] < this.elements[index]) {
+      this.swap(getParentIndex(index), index);
+      index = getParentIndex(index);
+    }
+  }
+  
+  bubbleDown() {
+    let index = 0;
+    while (this.hasLeftChild(index)) {
+      let smallerChildIndex = getLeftChildIndex(index);
+      if (this.hasRightChild(index) && this.rightChild(index) > this.leftChild(index)) {
+        smallerChildIndex = getRightChildIndex(index);
+      }
+      if (this.elements[index] > this.elements[smallerChildIndex]) {
+        break;
+      } else {
+        this.swap(smallerChildIndex, index);
+      }
+      index = smallerChildIndex;
+    }
+  }
+}
